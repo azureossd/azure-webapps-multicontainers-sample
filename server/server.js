ffe0807ef9988e55be451916cdcb703150140902
Edison Garcia Chinas <edisga@microsoft.com>
2020-07-27 21:47:09 -0500

    Fixing port:

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const directoryPath = "/data";
 
