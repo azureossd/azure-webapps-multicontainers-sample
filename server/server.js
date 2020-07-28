28c325b896f241e4c76402b0f4d3f2966c099b2c
Edison Garcia Chinas <edisga@microsoft.com>
2020-07-27 21:39:55 -0500

    Changing SSH Ciphers

--- server/server.js
+++ server/server.js
@@ -2,20 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fs = require('fs')
-const port = 3001
+const port = process.env.PORT || 3000;
 
 const directoryPath = "/data";
 
 app.get('/api/',function(req,res){
-    res.json({ status: 'Response from api.'});
+    res.json({ status: 'Response from server API.'});
 });
 
 app.get('/api/items/',function(req,res){
+
     var items = [];
+
     fs.readdir(directoryPath, function (err, files) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         } 
+        
         files.forEach(function (file) {
             items.push(file) 
         });
